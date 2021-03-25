b90f8a48b475133b5974a755298de438cbc89a73
Promise Fru <promisefru1@gmail.com>
2021-03-25 12:12:18 +0100

    created /users/stored_tokens

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,9 @@ var User = db.users;
 const {
     v4: uuidv4
 } = require('uuid');
+const {
+    Op
+} = require("sequelize");
 
 module.exports = (app) => {
     app.post("/users/profile", async (req, res, next) => {
@@ -34,5 +37,33 @@ module.exports = (app) => {
         return next(error);
     });
 
-    
+    app.post("/users/stored_tokens", async (req, res, next) => {
+        let user = await User.findOne({
+            where: {
+                [Op.and]: [{
+                    auth_key: req.body.auth_key
+                }, {
+                    id: req.body.user_id
+                }]
+            }
+        })
+
+        if (!user) {
+            const error = new Error("Invalid user");
+            error.httpStatusCode = 401;
+            return next(error);
+        }
+
+        let token = await user.getOauth2s();
+        let userData = {
+            token: {
+                access_token: token[0].accessToken,
+                refresh_token: token[0].refreshToken,
+                expiry_date: token[0].expiry_date,
+                scope: token[0].scope
+            }
+        };
+
+        return res.status(200).json(userData);
+    })
 }
