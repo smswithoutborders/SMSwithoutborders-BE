d9bc00c7b5f3e2072a7f4b201d5c93d544eaecf0
Promise Fru <promisefru1@gmail.com>
2021-04-02 14:01:04 +0100

    revert

--- controllers/googleAuth.js
+++ controllers/googleAuth.js
@@ -39,8 +39,8 @@ module.exports = (app) => {
         iden.id = req.query.iden;
         iden.proId = req.query.provider
         // Opens the URL in the default browser.
-        // await open(url);
-        res.redirect(url);
+        await open(url);
+        // res.redirect(url);
     });
 
     app.get('/oauth2/google/Tokens/redirect', async (req, res, next) => {
