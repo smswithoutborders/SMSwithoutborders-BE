a4eed6ac71ae2457bef3249b15a1b89b4eb94de9
Promise Fru <promisefru1@gmail.com>
2021-04-02 19:25:37 +0100

    adding comments to code

--- routes/routes.js
+++ routes/routes.js
@@ -12,6 +12,7 @@ const {
 module.exports = (app) => {
     app.post("/users/profiles", async (req, res, next) => {
         if (req.body.phone_number) {
+            // SEARCH FOR USER IN DB
             let user = await User.findAll({
                 where: {
                     phone_number: req.body.phone_number
@@ -21,18 +22,21 @@ module.exports = (app) => {
                 return next(error);
             });
 
+            // RETURN = [], IF NO USER FOUND
             if (user.length < 1) {
                 const error = new Error("phone number doesn't exist");
                 error.httpStatusCode = 401;
                 return next(error);
             }
 
+            // IF RETURN HAS MORE THAN ONE ITEM
             if (user.length > 1) {
                 const error = new Error("duplicate phone number");
                 error.httpStatusCode = 409;
                 return next(error);
             }
 
+            // CREATE AUTH_KEY ON LOGIN
             await user[0].update({
                 auth_key: uuidv4()
             }).catch(error => {
@@ -40,23 +44,28 @@ module.exports = (app) => {
                 return next(error);
             });
 
+            // RETURN AUTH_KEY
             return res.status(200).json({
                 auth_key: user[0].auth_key
             });
         }
 
+        // IF PHONE NUMBER FIELD IS EMPTY
         const error = new Error("phone number cannot be empty");
         error.httpStatusCode = 400;
         return next(error);
     });
 
     app.post("/users/stored_tokens", async (req, res, next) => {
+        // ==================== REQUEST BODY CHECKS ====================
         if (!req.body.auth_key) {
             const error = new Error("auth_key cannot be empty");
             error.httpStatusCode = 400;
             return next(error);
         };
+        // =============================================================
 
+        // SEARCH FOR USER IN DB
         let user = await User.findAll({
             where: {
                 auth_key: req.body.auth_key
@@ -66,38 +75,47 @@ module.exports = (app) => {
             return next(error);
         });
 
+        // RETURN = [], IF USER NOT FOUND
         if (user.length < 1) {
             const error = new Error("Invalid key");
             error.httpStatusCode = 401;
             return next(error);
         }
 
+        // IF RETURN HAS MORE THAN ONE ITEM
         if (user.length > 1) {
             const error = new Error("duplicate Users");
             error.httpStatusCode = 409;
             return next(error);
         }
 
+        // GET ALL TOKENS UNDER CURRENT USER
         let token = await user[0].getOauth2s();
 
+        // RETURN = [], IF NO TOKEN EXIST UNDER CURRENT USER
         if (token.length < 1) {
             return res.status(200).json([]);
         }
 
-        // store tokens from db
         let userData = {
             user_token: []
         }
 
-        // filter tokens by provider
+        // FILTER TOKENS BY PROVIDER
         if (req.body.provider) {
+            // LOOP THROUGH ALL TOKENS FOUND
             for (let i = 0; i < token.length; i++) {
+                // GET REQUESTED PROVIDER FOR CURRENT TOKEN
                 let provider = await token[i].getProvider({
                     where: {
                         name: `${req.body.provider}`
                     }
+                }).catch(error => {
+                    error.httpStatusCode = 500
+                    return next(error);
                 });
 
+                // IF PROVIDER FOUND
                 if (provider) {
                     userData.user_token.push({
                         provider: provider.name,
@@ -111,13 +129,19 @@ module.exports = (app) => {
                     })
                 }
             }
+            // RETURN STORED TOKEN AND PROVIDER
             return res.status(200).json(userData)
         }
 
-        // get all tokens
+        // LOOP THROUGH ALL TOKENS FOUND
         for (let i = 0; i < token.length; i++) {
-            let provider = await token[i].getProvider();
+            // GET ALL TOKENS UNDER CURRENT USER
+            let provider = await token[i].getProvider().catch(error => {
+                error.httpStatusCode = 500
+                return next(error);
+            });
 
+            // IF PROVIDER FOUND
             if (provider) {
                 userData.user_token.push({
                     provider: provider.name,
@@ -131,10 +155,13 @@ module.exports = (app) => {
                 })
             }
         }
+
+        // RETURN STORED TOKENS AND PROVIDER
         return res.status(200).json(userData)
     })
 
     app.post("/users/tokens", async (req, res, next) => {
+        // ==================== REQUEST BODY CHECKS ====================
         if (!req.body.auth_key) {
             const error = new Error("auth_key cannot be empty");
             error.httpStatusCode = 400;
@@ -152,7 +179,9 @@ module.exports = (app) => {
             error.httpStatusCode = 400;
             return next(error);
         };
+        // ===============================================================
 
+        // SEARCH FOR PROVIDER AND PLATFORM IN DB
         let provider = await Providers.findAll({
             where: {
                 [Op.and]: [{
@@ -166,18 +195,21 @@ module.exports = (app) => {
             return next(error);
         });
 
+        // RETURN = [], IF PROVIDER NOT FOUND
         if (provider.length < 1) {
             const error = new Error("invalid provider or platform");
             error.httpStatusCode = 401;
             return next(error);
         }
 
+        // IF PROVIDER IS MORE THAN ONE IN DB
         if (provider.length > 1) {
             const error = new Error("Duplicate provider");
             error.httpStatusCode = 409;
             return next(error);
         }
 
+        // SEARCH FOR USER IN DB
         let user = await User.findAll({
             where: {
                 auth_key: req.body.auth_key
@@ -187,18 +219,21 @@ module.exports = (app) => {
             return next(error);
         })
 
+        // RTURN = [], IF USER IS NOT FOUND
         if (user.length < 1) {
             const error = new Error("Invalid key");
             error.httpStatusCode = 401;
             return next(error);
         }
 
+        // IF MORE THAN ONE USER EXIST IN DATABASE
         if (user.length > 1) {
             const error = new Error("duplicate Users");
             error.httpStatusCode = 409;
             return next(error);
         }
 
+        // RETURN FOUND USER AND PROVIDER
         return res.redirect(`/oauth2/${provider[0].name}/Tokens/?iden=${user[0].id}&provider=${provider[0].id}`);
     });
 
