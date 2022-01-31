e410d7eee50d3995022f15dcf154afef9e64cbda
Promise Fru <promisefru1@gmail.com>
2022-01-31 13:18:37 +0100

    updated sample configs

--- models/store_sessions.models.js
+++ models/store_sessions.models.js
@@ -1,13 +1,17 @@
 const ERRORS = require("../error.js");
 const db = require("../schemas");
 
+const config = require('config');
+const SERVER_CFG = config.get("SERVER");
+const secure = SERVER_CFG.api.secure_sessions;
+
 var Session = db.sessions;
 
 module.exports = async (userId, user_agent) => {
     const hour = 2 * 60 * 60 * 1000;
     const data = {
         maxAge: hour,
-        secure: false,
+        secure: secure,
         httpOnly: true,
         sameSite: 'lax'
     }
