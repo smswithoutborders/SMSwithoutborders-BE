b3063c5a6a0cccb1ca79cfd03bf5a3d8fe233385
Promise Fru <promisefru1@gmail.com>
2022-03-02 14:52:28 +0100

    update: updated DELETE /users/:user_id

--- routes/user_management/v2.js
+++ routes/user_management/v2.js
@@ -648,7 +648,7 @@ router.post("/users/:user_id/recovery",
     VALIDATOR.userAgent,
     VALIDATOR.cookies,
     VALIDATOR.newPassword,
-    async (req, res, next) => {
+    async (req, res) => {
         try {
             // Finds the validation errors in this request and wraps them in an object with handy functions
             const errors = validationResult(req);
@@ -714,70 +714,68 @@ router.post("/users/:user_id/recovery",
         }
     });
 
-router.delete("/users/:user_id", async (req, res, next) => {
-    try {
-        if (!req.params.user_id) {
-            logger.error("NO USERID");
-            throw new ERRORS.BadRequest();
+router.delete("/users/:user_id",
+    VALIDATOR.userId,
+    VALIDATOR.cookies,
+    VALIDATOR.password,
+    VALIDATOR.userAgent,
+    async (req, res) => {
+        try {
+            // Finds the validation errors in this request and wraps them in an object with handy functions
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                errors.array().map(err => {
+                    if (err.param == "SWOB") {
+                        logger.error(`${err.param}: ${err.msg}`);
+                        throw new ERRORS.Unauthorized();
+                    }
+                    logger.error(`${err.param}: ${err.msg}`);
+                });
+                throw new ERRORS.BadRequest();
+            }
+            // =============================================================
+
+            const SID = req.cookies.SWOB.sid;
+            const UID = req.params.user_id;
+            const COOKIE = req.cookies.SWOB.cookie;
+            const USER_AGENT = req.get("user-agent");
+            const PASSWORD = req.body.password;
+
+            const ID = await FIND_SESSION(SID, UID, USER_AGENT, null, null, null, COOKIE);
+            const USER = await VERIFY_PASSWORDS(ID, PASSWORD);
+            let GRANTS = await USER.getWallets();
+            const originalURL = req.header("Origin");
+
+            for (let i = 0; i < GRANTS.length; i++) {
+                let PLATFORM = await VERIFY_PLATFORMS(GRANTS[i].platformId)
+                let GRANT = await PURGE_GRANTS(originalURL, PLATFORM.name, GRANTS[i], USER);
+                await DELETE_GRANTS(GRANT);
+            };
+
+            await DELETE_ACCOUNTS(USER);
+
+            return res.status(200).json();
+        } catch (err) {
+            if (err instanceof ERRORS.BadRequest) {
+                return res.status(400).send(err.message);
+            } // 400
+            if (err instanceof ERRORS.Forbidden) {
+                return res.status(403).send(err.message);
+            } // 403
+            if (err instanceof ERRORS.Unauthorized) {
+                return res.status(401).send(err.message);
+            } // 401
+            if (err instanceof ERRORS.Conflict) {
+                return res.status(409).send(err.message);
+            } // 409
+            if (err instanceof ERRORS.NotFound) {
+                return res.status(404).send(err.message);
+            } // 404
+
+            logger.error(err);
+            return res.status(500).send("internal server error");
         }
-        if (!req.cookies.SWOB) {
-            logger.error("NO COOKIE");
-            throw new ERRORS.Unauthorized();
-        };
-        // ==================== REQUEST BODY CHECKS ====================
-        if (!req.body.password) {
-            logger.error("NO PASSWORD");
-            throw new ERRORS.BadRequest();
-        };
-
-        // TODO ADD MIDDLEWARE CHECKS
-        if (req.body.password.length < 8) {
-            logger.error("PASSWORD < 8 CHARS");
-            throw new ERRORS.BadRequest();
-        };
-
-        // =============================================================
-        const SID = req.cookies.SWOB.sid;
-        const UID = req.params.user_id;
-        const COOKIE = req.cookies.SWOB.cookie;
-        const USER_AGENT = req.get("user-agent");
-        const PASSWORD = req.body.password;
-
-        const ID = await FIND_SESSION(SID, UID, USER_AGENT, null, null, null, COOKIE);
-        const USER = await VERIFY_PASSWORDS(ID, PASSWORD);
-        let GRANTS = await USER.getWallets();
-        const originalURL = req.header("Origin");
-
-        for (let i = 0; i < GRANTS.length; i++) {
-            let PLATFORM = await VERIFY_PLATFORMS(GRANTS[i].platformId)
-            let GRANT = await PURGE_GRANTS(originalURL, PLATFORM.name, GRANTS[i], USER);
-            await DELETE_GRANTS(GRANT);
-        };
-
-        await DELETE_ACCOUNTS(USER);
-
-        return res.status(200).json();
-    } catch (err) {
-        if (err instanceof ERRORS.BadRequest) {
-            return res.status(400).send(err.message);
-        } // 400
-        if (err instanceof ERRORS.Forbidden) {
-            return res.status(403).send(err.message);
-        } // 403
-        if (err instanceof ERRORS.Unauthorized) {
-            return res.status(401).send(err.message);
-        } // 401
-        if (err instanceof ERRORS.Conflict) {
-            return res.status(409).send(err.message);
-        } // 409
-        if (err instanceof ERRORS.NotFound) {
-            return res.status(404).send(err.message);
-        } // 404
-
-        logger.error(err);
-        return res.status(500).send("internal server error");
-    }
-});
+    });
 
 router.post("/users/:user_id/verify", async (req, res, next) => {
     try {
