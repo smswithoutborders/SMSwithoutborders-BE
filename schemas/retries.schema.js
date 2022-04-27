5f1b5c01cde6d08d1367f1747214f016e792825d
Promise Fru <promisefru1@gmail.com>
2022-04-27 12:01:00 +0100

    adjusted login retries counter

--- schemas/retries.schema.js
+++ schemas/retries.schema.js
@@ -2,7 +2,8 @@ module.exports = (sequelize, Sequelize) => {
     let Retry = sequelize.define("retries", {
         uniqueId: Sequelize.STRING,
         count: Sequelize.INTEGER,
-        block: Sequelize.INTEGER
+        block: Sequelize.INTEGER,
+        expires: Sequelize.DATE
     });
 
     return Retry;
