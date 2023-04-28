56542683122b103139f4b51576b3d697c370a7d0
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2023-04-28 11:29:10 +0100

    docs: included src path in python path for sphinx and added env file import for swob env vars

--- sphinx/docs/source/conf.py
+++ sphinx/docs/source/conf.py
@@ -17,16 +17,59 @@ release = '1.0.0'
 # add src folder
 import os
 import sys
+# sys.path.insert(0, os.path.relpath('../../../src/'))
+
+# Add the parent directory of "src" to the Python path
+# sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../..'))
+
+# Path to the module you want to document
 sys.path.insert(0, os.path.abspath('../../../src'))
+# sys.path.insert(0, os.path.abspath('../../../src/models'))
+
+# env files needed for swob
+from dotenv import load_dotenv
+from pathlib import Path
+
+dotenv_path = Path('../../.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 extensions = [
     "myst_parser",
     "sphinx.ext.duration", # show build time
     "sphinx.ext.autosectionlabel", # generate targets for every subheading in every document
+    "sphinx.ext.autodoc", # auto gen docs from code
+    "sphinx.ext.napoleon", # for other docstring formats
+    "sphinx.ext.viewcode", #
+    # "sphinx.ext.autosummary", # doc of all code
 ]
 
+# attempt  to showing __init__ docs
+# autodoc_default_options = {
+#     'members': True,
+#     'member-order': 'bysource',
+#     'special-members': '__init__',
+#     'undoc-members': True,
+#     'exclude-members': '__weakref__'
+# }
+
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
+    "inherited-members": True,
+    "show-inheritance": True,
+    # "private-members": False,
+    # "imported-members": False,
+    "ignore-module-all": False,
+    # "exclude-protected": False,
+    "members": True,  # include class members
+}
+
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_2FA.py']
 
 
 # -- Options for HTML output -------------------------------------------------
