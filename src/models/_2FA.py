c1a3ef39e758887d382c1ccd74af7664d289c224
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2023-05-02 22:33:01 +0100

    doc: added documentation for the OTP_Model class (models._2FA)

--- src/models/_2FA.py
+++ src/models/_2FA.py
@@ -1,3 +1,10 @@
+from werkzeug.exceptions import TooManyRequests
+from werkzeug.exceptions import Forbidden
+from werkzeug.exceptions import InternalServerError
+from datetime import timedelta
+from datetime import datetime
+from src.schemas.svretries import Svretries
+from settings import Configurations
 import logging
 
 from twilio.rest import Client
@@ -6,7 +13,6 @@ from twilio.base.exceptions import TwilioRestException
 logger = logging.getLogger(__name__)
 
 # configurations
-from settings import Configurations
 twilio_account_sid = Configurations.TWILIO_ACCOUNT_SID
 twilio_auth_token = Configurations.TWILIO_AUTH_TOKEN
 twilio_service_sid = Configurations.TWILIO_SERVICE_SID
@@ -15,20 +21,21 @@ duration_2 = Configurations.SECOND_RESEND_DURATION
 duration_3 = Configurations.THIRD_RESEND_DURATION
 duration_4 = Configurations.FOURTH_RESEND_DURATION
 
-from src.schemas.svretries import Svretries
-
-from datetime import datetime
-from datetime import timedelta
-
-from werkzeug.exceptions import InternalServerError
-from werkzeug.exceptions import Forbidden
-from werkzeug.exceptions import TooManyRequests
 
 SMSObject = ()
 
+
 class OTP_Model:
-    def __init__(self, phone_number:str) -> None:
-        """
+    """
+    This class implements a model for One-Time Password (OTP) verification
+    using the Twilio Verify API.
+    """
+
+    def __init__(self, phone_number: str) -> None:
+        """Initializes an OTP_Model instance.
+
+        Args:
+            phone_number (str): The phone number to send OTPs to.
         """
         self.client = Client(twilio_account_sid, twilio_auth_token)
         self.phone_number = phone_number
@@ -36,15 +43,19 @@ class OTP_Model:
 
     def verification(self) -> SMSObject:
         """
+        Starts an OTP verification process using the Twilio Verify API.
+
+        Returns:
+            SMSObject: An object representing the Twilio verification process.
         """
         try:
             logger.debug("Starting OTP verification ...")
 
             verification = self.client.verify \
-                        .v2 \
-                        .services(twilio_service_sid) \
-                        .verifications \
-                        .create(to=self.phone_number, channel='sms')
+                .v2 \
+                .services(twilio_service_sid) \
+                .verifications \
+                .create(to=self.phone_number, channel='sms')
 
             logger.info("- OTP verification complete")
 
@@ -55,15 +66,23 @@ class OTP_Model:
 
     def verification_check(self, code: str) -> SMSObject:
         """
+        Checks an OTP verification code using the Twilio Verify API.
+
+        Args:
+            code (str): The OTP verification code to check.
+
+        Returns:
+            SMSObject: An object representing the result of the Twilio verification
+            check.
         """
         try:
             logger.debug("Starting OTP verification ...")
 
             verification_check = self.client.verify \
-                        .v2 \
-                        .services(twilio_service_sid) \
-                        .verification_checks \
-                        .create(to=self.phone_number, code=code)
+                .v2 \
+                .services(twilio_service_sid) \
+                .verification_checks \
+                .create(to=self.phone_number, code=code)
 
             logger.info("- OTP verification complete")
 
@@ -78,14 +97,26 @@ class OTP_Model:
 
     def check_count(self, unique_id: str, user_id: str):
         """
+        Checks the number of OTP verification attempts for a user.
+
+        Args:
+            unique_id (str): A unique identifier for the verification attempt.
+            user_id (str): The user ID associated with the verification attempt.
+
+        Returns:
+            src.schemas.svretries.Svretries: A database model instance representing
+            the number of OTP verification attempts for the user.
         """
         try:
-            logger.debug("Finding SMS resend record for %s using %s ..." % (user_id, unique_id))
+            logger.debug("Finding SMS resend record for %s using %s ..." %
+                         (user_id, unique_id))
 
-            counter = self.Svretries.get(self.Svretries.userId == user_id, self.Svretries.uniqueId == unique_id)
+            counter = self.Svretries.get(
+                self.Svretries.userId == user_id, self.Svretries.uniqueId == unique_id)
 
         except self.Svretries.DoesNotExist:
-            logger.debug("Creating SMS resend record for %s using %s ..." % (user_id, unique_id))
+            logger.debug("Creating SMS resend record for %s using %s ..." % (
+                user_id, unique_id))
 
             new_counter = self.Svretries.create(
                 uniqueId=unique_id,
@@ -93,13 +124,14 @@ class OTP_Model:
                 count=0,
                 expires=None
             )
-            
+
             logger.info("- Successfully created SMS resend record")
 
             return new_counter
 
         else:
-            logger.debug("Checking SMS resend record for %s using %s ..." % (user_id, unique_id))
+            logger.debug("Checking SMS resend record for %s using %s ..." % (
+                user_id, unique_id))
 
             expires = counter.expires
 
@@ -115,7 +147,7 @@ class OTP_Model:
                 logger.debug("Resetting count ...")
 
                 upd_counter = self.Svretries.update(
-                    count = 0
+                    count=0
                 ).where(
                     self.Svretries.uniqueId == unique_id,
                     self.Svretries.userId == user_id
@@ -131,19 +163,30 @@ class OTP_Model:
 
     def add_count(self, counter) -> str:
         """
+        Adds to the count of verification attempts and updates the expiry date
+        for a user's OTP verification.
+
+        Args:
+            counter (src.schemas.svretries.Svretries): A database record instance representing
+            the number of OTP verification attempts for the user.
+
+        Returns:
+            str: A timestamp representing the expiry date for the user's OTP verification.
         """
+
         unique_id = counter.uniqueId
         user_id = counter.userId
         count = counter.count
 
-        logger.debug("Adding SMS resend record for %s using %s ..." % (user_id, unique_id))
+        logger.debug("Adding SMS resend record for %s using %s ..." %
+                     (user_id, unique_id))
 
         if count+1 == 1:
             expires = datetime.now() + timedelta(milliseconds=duration_1)
 
             upd_counter = self.Svretries.update(
-                count = count+1,
-                expires = expires
+                count=count+1,
+                expires=expires
             ).where(
                 self.Svretries.uniqueId == unique_id,
                 self.Svretries.userId == user_id
@@ -158,8 +201,8 @@ class OTP_Model:
             expires = datetime.now() + timedelta(milliseconds=duration_2)
 
             upd_counter = self.Svretries.update(
-                count = count+1,
-                expires = expires
+                count=count+1,
+                expires=expires
             ).where(
                 self.Svretries.uniqueId == unique_id,
                 self.Svretries.userId == user_id
@@ -174,8 +217,8 @@ class OTP_Model:
             expires = datetime.now() + timedelta(milliseconds=duration_3)
 
             upd_counter = self.Svretries.update(
-                count = count+1,
-                expires = expires
+                count=count+1,
+                expires=expires
             ).where(
                 self.Svretries.uniqueId == unique_id,
                 self.Svretries.userId == user_id
@@ -190,8 +233,8 @@ class OTP_Model:
             expires = datetime.now() + timedelta(milliseconds=duration_4)
 
             upd_counter = self.Svretries.update(
-                count = count+1,
-                expires = expires
+                count=count+1,
+                expires=expires
             ).where(
                 self.Svretries.uniqueId == unique_id,
                 self.Svretries.userId == user_id
@@ -205,7 +248,11 @@ class OTP_Model:
 
     def delete_count(self, counter_id: int):
         """
-        """ 
+        Deletes the SMS resend record counter
+
+        Args:
+          counter_id (int): An integer which identifies the counter for a given retry record
+        """
         try:
             logger.debug("Finding SMS resend record %s ..." % counter_id)
 
@@ -220,7 +267,8 @@ class OTP_Model:
             unique_id = counter.uniqueId
             user_id = counter.userId
 
-            logger.debug("deleting SMS resend record for %s using %s ..." % (user_id, unique_id))
+            logger.debug("deleting SMS resend record for %s using %s ..." % (
+                user_id, unique_id))
 
             counter.delete_instance()
 
